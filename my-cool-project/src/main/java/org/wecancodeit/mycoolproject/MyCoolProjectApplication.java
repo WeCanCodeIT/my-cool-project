09a67c7adffbb29de1eeb99be71137bb6da9646a
Columbus Instructor <instructor@wecancodeit.org>
2019-03-29 15:04:49 -0400

    initial commit

---
+++ my-cool-project/src/main/java/org/wecancodeit/mycoolproject/MyCoolProjectApplication.java
@@ -0,0 +1,13 @@
+package org.wecancodeit.mycoolproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyCoolProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyCoolProjectApplication.class, args);
+	}
+
+}
---
+++ my-cool-project/src/test/java/org/wecancodeit/mycoolproject/MyCoolProjectApplicationTests.java
@@ -0,0 +1,16 @@
+package org.wecancodeit.mycoolproject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MyCoolProjectApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
